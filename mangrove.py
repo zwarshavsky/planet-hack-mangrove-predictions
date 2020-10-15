2ea44a1aa1c6366f60487598b2dcec2dd6c8a470
Siddharth Gadepalli <siddharthgadepalli@gmail.com>
2020-10-15 16:03:23 -0500

    added stats url and modularized auth object method;

--- mangrove.py
+++ mangrove.py
@@ -8,20 +8,34 @@ class MangrovePlanet():
         self.planet_api_key = PLANET_API_KEY
         self.url = 'https://api.planet.com/data/v1'
     
-    def auth_obj(self):
+    def send_planet_request(self, request_type, method, request=None):
         
         try:
             session = requests.session()
             session.auth = (PLANET_API_KEY, "")
-            res = session.get(self.url)
-            status_code = res.status_code
+            url = self.url + str(request_type)
+            if request is None and method == "get":
+                res = session.get(url)
+                status_code = res.status_code
+            elif request is not None and method == "post":
+                res = session.post(url, json=request)
+                status_code = res.status_code
         except Exception as ex:
             print(ex)
             print("Authorization Failed")
         return res, status_code
     
-    def fetch_data(self):
-        res, status_code = self.auth_obj()
+    def fetch_stats_data(self):
+
+        try:
+            item_types = ["PSScene3Band", "REOrthoTile"]
+            date_filter = {"type": "DateRangeFilter", "field_name": "acquired", "config": {"gte": "2013-01-01T00:00:00.000Z"}}
+            request = {"item_types" : item_types, "interval" : "year", "filter" : date_filter}
+            res, status_code = self.send_planet_request(request_type="/stats", method="post", request=request)
+        except Exception as ex:
+            print(ex)
+            print("Request failed")
+
         return res, status_code
 
 if __name__ == "__main__":
